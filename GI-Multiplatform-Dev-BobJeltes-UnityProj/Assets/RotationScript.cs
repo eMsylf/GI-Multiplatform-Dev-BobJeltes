a474529e9c0a851d9d5447b7d7bc75df6486a85a
Bob <bobjeltes@hotmail.com>
2019-01-07 11:27:25 +0100

    Whoa buddy
    bottom text

---
+++ GI-Multiplatform-Dev-BobJeltes-UnityProj/Assets/RotationScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour {
+
+    [Range(1, 100)]
+    public float rotationSpeed;
+    private float halfScreenHeight = 2048/2;
+
+    float swipePosPrevious;
+    float swipePosCurrent;
+    public float swipePosDelta;
+
+    void Start() {
+
+    }
+
+
+	void Update () {
+
+
+        
+        if (Input.touchCount > 0) {
+
+            swipePosCurrent = Input.mousePosition.y;
+
+            swipePosDelta = swipePosCurrent - swipePosPrevious;
+
+            swipePosPrevious = swipePosCurrent;
+        }
+
+        //rotationSpeed = (Input.mousePosition.y - halfScreenHeight) / 100;
+        
+
+        transform.Rotate(Vector3.right * swipePosDelta / -20);
+
+
+        
+
+	}
+}
